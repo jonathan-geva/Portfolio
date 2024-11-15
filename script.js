5f8216dbc02da287cf73da42ea569dadf890035f
jonathan-geva <jonathangeva@gmail.com>
2024-11-15 16:24:39 +0100

    Fixed menu and added Intranet also updated info about me

--- script.js
+++ script.js
@@ -22,3 +22,10 @@ menu.addEventListener('click', (e) => {
         menu.classList.remove('show-menu');
     }
 });
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
