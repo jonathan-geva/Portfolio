317066dd7b4f2a0c147cc1633dbdf4c01e1a386f
jonathan-geva <jonathangeva@gmail.com>
2024-11-15 07:12:08 +0100

    Added Menu and updated Design

---
+++ script.js
@@ -0,0 +1,24 @@
+// script.js
+const menuLinks = document.querySelectorAll('.menu a');
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const sectionId = link.getAttribute('href');
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const menu = document.getElementById('menu');
+
+hamburgerIcon.addEventListener('click', () => {
+    menu.classList.toggle('show-menu');
+});
+
+menu.addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu')) {
+        menu.classList.remove('show-menu');
+    }
+});
